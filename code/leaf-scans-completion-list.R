2c95c347e30ded169366daee9c726e58daec9c66
aekendig <aekendig@gmail.com>
2019-08-07 19:12:02 -0400

    create functions for leaf scan completion list

--- code/leaf-scans-completion-list.R
+++ code/leaf-scans-completion-list.R
@@ -2,7 +2,7 @@
 
 # file: leaf-scans-completion-list-2019
 # author: Amy Kendig
-# date last edited: 6/30/19
+# date last edited: 8/7/19
 # goal: check that leaf scans downloaded from One Drive (initial storage space) are complete
 
 
@@ -14,68 +14,150 @@ rm(list=ls())
 # load packages
 library(tidyverse)
 
-# retrieve list of files
-jun19 <- tibble(
+# retrieve list of scans
+scans_jn <- tibble(
   files = list.files("../Leaf Scans/Leaf Scans June 2019")
+)
+scans_jl <- tibble(
+  files = list.files("..../Downloads/Leaf Scans June 2019")
 )
 
 # data collection sheet
-djn <- read_csv("./data/focal-disease-jun-2019-density-exp.csv")
+field_jn <- read_csv("./data/focal-disease-jun-2019-density-exp.csv")
 
 
 #### edit data ####
 
-# separate experiments
-jn19de <- jun19 %>%
-  filter(substr(files, 1, 1) == "D")
+## functions for processing scan data by experiment
 
-jn19le <- jun19 %>%
-  filter(substr(files, 1, 1) == "L")
+# density experiment
+dens_fun <- function(dat) {
+  dat_out <- dat %>%
+    filter(substr(files, 1, 1) == "D") %>%
+    mutate(
+      site = substr(files, 1, 2),
+      plot = substr(files, 4, 5) %>% gsub("[^[:digit:]]", "", .) %>% as.numeric(),
+      treatment = substr(files, 5, 6) %>% gsub("[^[:alpha:]]", "", .) %>% recode("F" = "fungicide", "W" = "water"),
+      sp = ifelse(grepl("Ev", files, fixed = T), "Ev", "Mv"),
+      ID = case_when(sp == "Ev" ~ substr(files, 10, 11) %>% gsub(c("_"), "", .),
+                     sp == "Mv" ~ ifelse(grepl("Edge", files, fixed = T) | grepl("edge", files, fixed = T) , 
+                                         "Edge", 
+                                         substr(files, 10, 11) %>% gsub(c("_"), "", .))),
+      focal = ifelse(ID %in% c("1", "2", "3", "A"), 1, 0)
+    ) 
+  
+  return(dat_out)
+}
 
-# new columns
-jn19de <- jn19de %>%
-  mutate(
-    site = substr(files, 1, 2),
-    plot = substr(files, 4, 5) %>% gsub("[^[:digit:]]", "", .) %>% as.numeric(),
-    treatment = substr(files, 5, 6) %>% gsub("[^[:alpha:]]", "", .) %>% recode("F" = "fungicide", "W" = "water"),
-    sp = ifelse(grepl("Ev", files, fixed = T), "Ev", "Mv"),
-    ID = case_when(sp == "Ev" ~ substr(files, 10, 11) %>% gsub(c("_"), "", .),
-                   sp == "Mv" ~ ifelse(grepl("Edge", files, fixed = T), 
-                                       "Edge", 
-                                       substr(files, 10, 11) %>% gsub(c("_"), "", .))),
-    focal = ifelse(ID %in% c("1", "2", "3", "A"), 1, 0)
-  ) 
+# litter experiment
+litt_fun <- function(dat) {
+  dat_out <- dat %>%
+    filter(substr(files, 1, 1) == "L") %>%
+    mutate(
+      site = substr(files, 8, 9),
+      plot = substr(files, 11, 12),
+      treatment = substr(plot, 1, 1) %>% 
+        recode("R" = "removal", "A" = "addition", "C" = "control"),
+      replicate = substr(plot, 2, 2)
+      ) 
+  
+  return(dat_out)
+}
 
-# check that the above worked as expected
-unique(jn19de$site)
-unique(jn19de$plot) # one is missing plot number
-unique(jn19de$treatment)
-unique(jn19de$sp)
-unique(jn19de$ID)
-filter(jn19de, !(ID %in% c("1", "2", "3", "A", "Edge")))
-unique(jn19de$focal)
+# create experiment-specific datasets
+scans_jn_d <- dens_fun(scans_jn)
+scans_jn_l <- litt_fun(scans_jn)
 
 
 #### analyze data ####
 
-# extract plots
-plots <- djn %>%
-  select(site, plot, treatment) %>%
-  unique()
+# manually check litter data because it's small
+data.frame(scans_jn_l)
 
-# compare plots to scans
-plots %>% anti_join(jn19de)
+## functions to check that all scans are present, there are no unexpected scnas, and there are no duplicates
 
-# check that all edge scans are included
-plots %>% anti_join(filter(jn19de, ID == "Edge"))
+# field notes
+notes_fun <- function(field_dat) {
+  scan_notes <- field_dat %>%
+    filter(scan == 0) %>%
+    select(site, plot, treatment, sp, ID, field_notes) %>%
+    data.frame()
+  
+  print(scan_notes)
+}
 
-# see why scans weren't collected
-djn %>%
-  filter(scan == 0) %>%
-  select(field_notes) %>%
-  unique()
+# duplicate scans
+dup_fun <- function(scan_dat) {
+  dup_scans <- scan_dat %>%
+    group_by(site, plot, treatment, sp, ID) %>%
+    summarise(reps = n()) %>%
+    filter(reps > 1) %>%
+    data.frame()
 
-# check that all individual scans are included
-djn %>% 
-  filter(scan == 1) %>%
-  anti_join(filter(jn19de, ID != "Edge"))
+  ifelse(nrow(dup_scans) > 0, print(dup_scans), "no duplicate scans")
+}
+
+# edge Mv
+edge_fun <- function(field_dat, scan_dat) {
+  
+  plots_missing <- field_dat %>%
+    select(site, plot, treatment) %>%
+    unique() %>%
+    anti_join(filter(scan_dat, focal == 0)) %>%
+    data.frame()
+  
+  ifelse(nrow(plots_missing) > 0, print(plots_missing), print("all plots are accounted for"))
+  
+  unexpected_scans <- scan_dat %>%
+    filter(focal == 0) %>%
+    anti_join(select(field_dat, site, plot, treatment) %>% unique()) %>%
+    data.frame()
+  
+  ifelse(nrow(unexpected_scans) > 0, print(unexpected_scans), "no unexpected scans")
+}
+
+# focal Mv
+mv_fun <- function(field_dat, scan_dat) {
+  
+  leaves_missing <- field_dat %>%
+    filter(sp == "Mv" & scan == 1) %>%
+    select(site, plot, treatment, ID) %>%
+    anti_join(filter(scan_dat, sp == "Mv" & focal == 1)) %>%
+    data.frame()
+  
+  ifelse(nrow(leaves_missing) > 0, print(leaves_missing), print("all leaves are accounted for"))
+  
+  unexpected_scans <- scan_dat %>%
+    filter(sp == "Mv" & focal == 1) %>%
+    anti_join(filter(field_dat, sp == "Mv" & scan == 1) %>% select(site, plot, treatment, ID)) %>%
+    data.frame()
+  
+  ifelse(nrow(unexpected_scans) > 0, print(unexpected_scans), "no unexpected scans")
+}
+
+# focal Ev
+ev_fun <- function(field_dat, scan_dat) {
+  
+  leaves_missing <- field_dat %>%
+    filter(sp == "Ev" & scan == 1) %>%
+    select(site, plot, treatment, ID) %>%
+    anti_join(filter(scan_dat, sp == "Ev")) %>%
+    data.frame()
+  
+  ifelse(nrow(leaves_missing) > 0, print(leaves_missing), print("all leaves are accounted for"))
+  
+  unexpected_scans <- scan_dat %>%
+    filter(sp == "Ev") %>%
+    anti_join(filter(field_dat, sp == "Ev" & scan == 1) %>% select(site, plot, treatment, ID)) %>%
+    data.frame()
+  
+  ifelse(nrow(unexpected_scans) > 0, print(unexpected_scans), "no unexpected scans")
+}
+
+
+# June data
+notes_fun(field_jn)
+dup_fun(scans_jn_d)
+edge_fun(field_jn, scans_jn_d)
+mv_fun(field_jn, scans_jn_d)
+ev_fun(field_jn, scans_jn_d)
