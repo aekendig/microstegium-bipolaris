6cb2722a8272feb734cb92ac93c0561d194dcff8
aekendig <aekendig@gmail.com>
2021-03-16 14:34:01 -0500

    add random effects and separate fig panels to plot biomass

--- code/plot_biomass_density_2019_density_exp.R
+++ code/plot_biomass_density_2019_density_exp.R
@@ -2,7 +2,7 @@
 
 # file: plot_biomass_density_2019_density_exp
 # author: Amy Kendig
-# date last edited: 3/11/21
+# date last edited: 3/16/21
 # goal: plot-level biomass and density relationships
 
 
@@ -15,15 +15,16 @@ rm(list=ls())
 library(tidyverse)
 library(brms)
 library(tidybayes) # for mean_hdi
+library(cowplot)
 
-# import plot information
+# import data
 plotsD <- read_csv("data/plot_treatments_2018_2019_density_exp.csv")
-
-# biomass
 mvBioD2Dat <- read_csv("data/mv_biomass_seeds_2019_density_exp.csv")
 evBioD2Dat <- read_csv("data/ev_biomass_seeds_oct_2019_density_exp.csv")
 bgBioD2Dat <- read_csv("intermediate-data/bg_processed_biomass_2019_density_exp.csv") 
 # bg_biomass_data_processing_2019_density_exp.R
+envD1Dat <- read_csv("intermediate-data/covariates_2018_density_exp.csv") 
+# covariate_data_processing_2018_density_exp
 
 # load individual growth models
 load("output/mv_biomass_mv_density_model_2019_density_exp.rda")
@@ -90,22 +91,34 @@ plotBioD2Dat <- bgBioD2Dat %>%
 plotD2Dat <- plotDens %>%
   full_join(plotBioD2Dat) %>%
   select(site, plot, treatment, background, density_level, background_density, Mv_seedling_density:Ev_adult_density, Mv_seedling_biomass:Ev_adult_biomass) %>%
-  mutate(fungicide = ifelse(treatment == "fungicide", 1, 0))
+  mutate(fungicide = ifelse(treatment == "fungicide", 1, 0),
+         plotf = as.factor(plot)) %>%
+  left_join(envD1Dat %>%
+              select(site, plot, treatment, soil_moisture_oct.prop, canopy_cover.prop))
 
 # divide
 mvPlotD2Dat <- plotD2Dat %>%
-  filter(plot %in% 1:4)
+  filter(plot %in% 1:4) %>%
+  mutate(soil_moisture_oct.c = scale(soil_moisture_oct.prop, center = T, scale = F)[,1],
+         canopy_cover.c = scale(canopy_cover.prop, center = T, scale = F)[,1],
+         log_tot_bio = log(Mv_seedling_biomass))
 
 evSPlotD2Dat <- plotD2Dat %>%
-  filter(plot %in% c(1, 5:7))
+  filter(plot %in% c(1, 5:7)) %>%
+  mutate(soil_moisture_oct.c = scale(soil_moisture_oct.prop, center = T, scale = F)[,1],
+         canopy_cover.c = scale(canopy_cover.prop, center = T, scale = F)[,1],
+         log_tot_bio = log(Ev_seedling_biomass))
 
 evAPlotD2Dat <- plotD2Dat %>%
-  filter(plot %in% c(1, 8:10))
+  filter(plot %in% c(1, 8:10)) %>%
+  mutate(soil_moisture_oct.c = scale(soil_moisture_oct.prop, center = T, scale = F)[,1],
+         canopy_cover.c = scale(canopy_cover.prop, center = T, scale = F)[,1],
+         log_tot_bio = log(Ev_adult_biomass))
 
 
 #### initial visualizations ####
 
-# figures
+# biomass by density
 ggplot(mvPlotD2Dat, aes(Mv_seedling_density, Mv_seedling_biomass, color = treatment)) +
   stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
   stat_summary(geom = "point", fun = "mean", size = 2)
@@ -118,8 +131,55 @@ ggplot(evAPlotD2Dat, aes(Ev_adult_density, Ev_adult_biomass, color = treatment))
   stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
   stat_summary(geom = "point", fun = "mean", size = 2)
 
+# biomass by soil moisture
+ggplot(mvPlotD2Dat, aes(soil_moisture_oct.prop, Mv_seedling_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
 
-#### Mv model ####
+ggplot(evSPlotD2Dat, aes(soil_moisture_oct.prop, Ev_seedling_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
+
+ggplot(evAPlotD2Dat, aes(soil_moisture_oct.prop, Ev_adult_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
+
+# biomass by canopy cover
+ggplot(mvPlotD2Dat, aes(canopy_cover.prop, Mv_seedling_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
+
+ggplot(evSPlotD2Dat, aes(canopy_cover.prop, Ev_seedling_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
+
+ggplot(evAPlotD2Dat, aes(canopy_cover.prop, Ev_adult_biomass, color = treatment)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = F)
+
+# soil and canopy
+ggplot(plotD2Dat, aes(canopy_cover.prop, soil_moisture_oct.prop)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x)
+
+cor.test(~ canopy_cover.prop + soil_moisture_oct.prop, data = plotD2Dat)
+# not significantly correlated
+
+# soil moisture by density (because it was measured at the end of the Y1 growing season)
+ggplot(mvPlotD2Dat, aes(Mv_seedling_density, soil_moisture_oct.prop, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
+  stat_summary(geom = "point", fun = "mean", size = 2)
+
+ggplot(evSPlotD2Dat, aes(Ev_seedling_density, soil_moisture_oct.prop, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
+  stat_summary(geom = "point", fun = "mean", size = 2)
+
+ggplot(evAPlotD2Dat, aes(Ev_adult_density, soil_moisture_oct.prop, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
+  stat_summary(geom = "point", fun = "mean", size = 2)
+
+
+#### Mv models ####
 
 # priors
 mvPlotD2Dat %>%
@@ -137,12 +197,13 @@ plot(x, y, type = "l")
 # model
 mvBioDensMod <- brm(data = mvPlotD2Dat, family = gaussian,
                     bf(Mv_seedling_biomass ~ (Mv_seedling_density * b0)/(1 + alpha * Mv_seedling_density), 
-                       b0 ~ 0 + treatment, 
+                       b0 ~ 0 + treatment + (1|site), 
                        alpha ~ 0 + treatment, 
                        nl = T),
                     prior <- c(prior(gamma(14, 1), nlpar = "b0", lb = 0),
-                               prior(exponential(0.5), nlpar = "alpha", lb = 0)), # use default for sigma
-                    iter = 6000, warmup = 1000, chains = 3) 
+                               prior(exponential(0.5), nlpar = "alpha", lb = 0)), # use default for sigma and sd
+                    iter = 6000, warmup = 1000, chains = 3,
+                    control = list(adapt_delta = 0.99)) 
 
 summary(mvBioDensMod)
 pp_check(mvBioDensMod, nsamples = 100)
@@ -152,10 +213,11 @@ plot(mvBioDensMod)
 mvBioDensSim <- mvPlotD2Dat %>%
   select(treatment) %>%
   unique() %>%
+  mutate(site = "D5") %>%
   expand_grid(tibble(Mv_seedling_density = seq(0, 67, length.out = 100))) %>%
-  mutate(pred = fitted(mvBioDensMod, newdata = ., re_formula = NA)[, "Estimate"],
-         lower = fitted(mvBioDensMod, newdata = ., re_formula = NA)[, "Q2.5"],
-         upper = fitted(mvBioDensMod, newdata = ., re_formula = NA)[, "Q97.5"])
+  mutate(pred = fitted(mvBioDensMod, newdata = ., allow_new_levels = T)[, "Estimate"],
+         lower = fitted(mvBioDensMod, newdata = ., allow_new_levels = T)[, "Q2.5"],
+         upper = fitted(mvBioDensMod, newdata = ., allow_new_levels = T)[, "Q97.5"])
 
 # figure
 ggplot(mvPlotD2Dat, aes(Mv_seedling_density, Mv_seedling_biomass, color = treatment)) +
@@ -163,6 +225,21 @@ ggplot(mvPlotD2Dat, aes(Mv_seedling_density, Mv_seedling_biomass, color = treatm
   geom_line(data = mvBioDensSim, aes(y = pred)) +
   stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0) +
   stat_summary(geom = "point", fun = "mean", size = 2)
+
+# evaluate environmental covariates
+mvPlotD2Dat %>%
+  filter(plot == 1 & fungicide == 0) %>%
+  summarise(bio = mean(log_tot_bio))
+
+mvBioDensMod2 <- brm(data = mvPlotD2Dat, family = gaussian,
+                     log_tot_bio ~ plotf * fungicide + soil_moisture_oct.c + canopy_cover.c + (1|site), 
+                     prior <- c(prior(normal(3, 1), class = "Intercept"),
+                                prior(normal(0, 1), class = "b")), # use default for sigma
+                     iter = 6000, warmup = 1000, chains = 3,
+                     control = list(adapt_delta = 0.999)) 
+
+summary(mvBioDensMod2)
+#  no sig effect of soil moisture or canopy cover
 
 
 #### Ev seedling model ####
@@ -193,24 +270,34 @@ plot(x, y, type = "l")
 #                     iter = 6000, warmup = 1000, chains = 3) 
 # 407 divergences, fit is basically linear
 
+evSPlotD2Dat %>%
+  filter(plot == 1 & fungicide == 0) %>%
+  summarise(bio = mean(log_tot_bio))
+
 evSBioDensMod <- brm(data = evSPlotD2Dat, family = gaussian,
-                     log(Ev_seedling_biomass) ~ Ev_seedling_density * fungicide, 
+                     log_tot_bio ~ Ev_seedling_density * fungicide + soil_moisture_oct.c + canopy_cover.c + (1|site), 
                      prior <- c(prior(normal(0, 1), class = "Intercept"),
                                 prior(normal(0, 1), class = "b")), # use default for sigma
-                     iter = 6000, warmup = 1000, chains = 3) 
+                     iter = 6000, warmup = 1000, chains = 3,
+                     control = list(adapt_delta = 0.999)) 
 
 summary(evSBioDensMod)
-pp_check(evSBioDensMod, nsamples = 100)
-plot(evSBioDensMod)
+# no significant effect of soil or canopy
+
+evSBioDensMod2 <- update(evSBioDensMod, formula. = ~ . - soil_moisture_oct.c - canopy_cover.c)
+summary(evSBioDensMod2)
+pp_check(evSBioDensMod2, nsamples = 100)
+plot(evSBioDensMod2)
 
 # simulate values
 evSBioDensSim <- evSPlotD2Dat %>%
   select(treatment, fungicide) %>%
   unique() %>%
+  mutate(site = "D5") %>%
   expand_grid(tibble(Ev_seedling_density = seq(0, 19, length.out = 100))) %>%
-  mutate(pred = fitted(evSBioDensMod, newdata = ., re_formula = NA)[, "Estimate"] %>% exp(),
-         lower = fitted(evSBioDensMod, newdata = ., re_formula = NA)[, "Q2.5"] %>% exp(),
-         upper = fitted(evSBioDensMod, newdata = ., re_formula = NA)[, "Q97.5"] %>% exp())
+  mutate(pred = fitted(evSBioDensMod2, newdata = ., allow_new_levels = T)[, "Estimate"] %>% exp(),
+         lower = fitted(evSBioDensMod2, newdata = ., allow_new_levels = T)[, "Q2.5"] %>% exp(),
+         upper = fitted(evSBioDensMod2, newdata = ., allow_new_levels = T)[, "Q97.5"] %>% exp())
 
 # figure
 ggplot(evSPlotD2Dat, aes(Ev_seedling_density, Ev_seedling_biomass, color = treatment)) +
@@ -247,23 +334,29 @@ plot(x, y, type = "l")
 # huge error around estimates, almost linear
 
 evABioDensMod <- brm(data = evAPlotD2Dat, family = gaussian,
-                     log(Ev_adult_biomass) ~ Ev_adult_density * fungicide, 
+                     log(Ev_adult_biomass) ~ Ev_adult_density * fungicide + soil_moisture_oct.c + canopy_cover.c + (1|site), 
                      prior <- c(prior(normal(2, 1), class = "Intercept"),
                                 prior(normal(0, 1), class = "b")), # use default for sigma
-                     iter = 6000, warmup = 1000, chains = 3) 
+                     iter = 6000, warmup = 1000, chains = 3,
+                     control = list(adapt_delta = 0.9999)) 
 
 summary(evABioDensMod)
-pp_check(evABioDensMod, nsamples = 100)
-plot(evABioDensMod)
+# no significant effect of soil or canopy
+
+evABioDensMod2 <- update(evABioDensMod, formula. = ~ . - soil_moisture_oct.c - canopy_cover.c)
+summary(evABioDensMod2)
+pp_check(evABioDensMod2, nsamples = 100)
+plot(evABioDensMod2)
 
 # simulate values
 evABioDensSim <- evAPlotD2Dat %>%
   select(treatment, fungicide) %>%
   unique() %>%
+  mutate(site = "D5") %>%
   expand_grid(tibble(Ev_adult_density = seq(0, 9, length.out = 100))) %>%
-  mutate(pred = fitted(evABioDensMod, newdata = ., re_formula = NA)[, "Estimate"] %>% exp(),
-         lower = fitted(evABioDensMod, newdata = ., re_formula = NA)[, "Q2.5"] %>% exp(),
-         upper = fitted(evABioDensMod, newdata = ., re_formula = NA)[, "Q97.5"] %>% exp())
+  mutate(pred = fitted(evABioDensMod2, newdata = ., allow_new_levels = T)[, "Estimate"] %>% exp(),
+         lower = fitted(evABioDensMod2, newdata = ., allow_new_levels = T)[, "Q2.5"] %>% exp(),
+         upper = fitted(evABioDensMod2, newdata = ., allow_new_levels = T)[, "Q97.5"] %>% exp())
 
 # figure
 ggplot(evAPlotD2Dat, aes(Ev_adult_density, Ev_adult_biomass, color = treatment)) +
@@ -273,7 +366,7 @@ ggplot(evAPlotD2Dat, aes(Ev_adult_density, Ev_adult_biomass, color = treatment))
   stat_summary(geom = "point", fun = "mean", size = 2)
 
 
-#### figure ####
+#### combined figure ####
 
 # combine datasets
 figDat <- mvPlotD2Dat %>%
@@ -315,7 +408,7 @@ labDat <- simDat %>%
   mutate(treatment = "fungicide")
 
 # figure
-pdf("output/plot_biomass_density_2019_density_exp.pdf", width = 10.5, height = 5)
+pdf("output/plot_biomass_density_presentation_2019_density_exp.pdf", width = 10.5, height = 5)
 ggplot(figDat, aes(density, biomass, group = treatment)) +
   stat_summary(geom = "errorbar", fun.data = "mean_cl_boot", width = 0, position = position_dodge(0.3), aes(color = treatment)) +
   stat_summary(geom = "point", fun = "mean", size = 2,  position = position_dodge(0.3), aes(color = treatment)) +
@@ -341,6 +434,94 @@ ggplot(figDat, aes(density, biomass, group = treatment)) +
 dev.off()
 
 
+#### separate figure ####
+
+# change treatment levels
+mvPlotD2Dat2 <- mvPlotD2Dat %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+mvBioDensSim2 <- mvBioDensSim %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+
+evSPlotD2Dat2 <- evSPlotD2Dat %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+evSBioDensSim2 <- evSBioDensSim %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+
+evAPlotD2Dat2 <- evAPlotD2Dat %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+evABioDensSim2 <- evABioDensSim %>%
+  mutate(treatment = case_when(treatment == "water" ~ "water (control)",
+                               TRUE ~ "fungicide") %>%
+           fct_rev())
+
+# figure settings #
+fig_theme <- theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size = 8, color = "black"),
+        axis.title = element_text(size = 10),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank(),
+        legend.position = "none",
+        legend.background = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_blank())
+
+mv_fig <- ggplot(mvPlotD2Dat2, aes(Mv_seedling_density, Mv_seedling_biomass, group = treatment)) +
+  geom_ribbon(data = mvBioDensSim2, aes(y = pred, ymin = lower, ymax = upper, fill = treatment), alpha = 0.3, color = NA) +
+  geom_line(data = mvBioDensSim2, aes(y = pred, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0, position = position_dodge(1), aes(color = treatment)) +
+  stat_summary(geom = "point", fun = "mean", size = 2,  position = position_dodge(1), aes(color = treatment)) +
+  xlab(expression(paste(italic("M. vimineum"), " density (", m^-2, ")", sep = ""))) +
+  ylab(expression(paste(italic("M. vimineum"), " biomass (g ", m^-2, ")", sep = ""))) +
+  scale_color_viridis_d(end = 0.6, name = "Treatment") +
+  scale_fill_viridis_d(end = 0.6, name = "Treatment") +
+  fig_theme
+
+evS_fig <- ggplot(evSPlotD2Dat2, aes(Ev_seedling_density, Ev_seedling_biomass, group = treatment)) +
+  geom_ribbon(data = evSBioDensSim2, aes(y = pred, ymin = lower, ymax = upper, fill = treatment), alpha = 0.3, color = NA) +
+  geom_line(data = evSBioDensSim2, aes(y = pred, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0, position = position_dodge(0.3), aes(color = treatment)) +
+  stat_summary(geom = "point", fun = "mean", size = 2,  position = position_dodge(0.3), aes(color = treatment)) +
+  xlab(expression(paste(italic("E. virginicus"), " seedling density (", m^-2, ")", sep = ""))) +
+  ylab(expression(paste(italic("E. virginicus"), " seedling biomass (g ", m^-2, ")", sep = ""))) +
+  scale_color_viridis_d(end = 0.6) +
+  scale_fill_viridis_d(end = 0.6) +
+  fig_theme +
+  theme(legend.position = c(0.35, 0.88),
+        axis.title.y = element_text(size = 10, hjust = -1),
+        axis.title.x = element_text(size = 10, hjust = 0.85))
+
+evA_fig <- ggplot(evAPlotD2Dat2, aes(Ev_adult_density, Ev_adult_biomass, group = treatment)) +
+  geom_ribbon(data = evABioDensSim2, aes(y = pred, ymin = lower, ymax = upper, fill = treatment), alpha = 0.3, color = NA) +
+  geom_line(data = evABioDensSim2, aes(y = pred, color = treatment)) +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0, position = position_dodge(0.1), aes(color = treatment)) +
+  stat_summary(geom = "point", fun = "mean", size = 2,  position = position_dodge(0.1), aes(color = treatment)) +
+  xlab(expression(paste(italic("E. virginicus"), " adult density (", m^-2, ")", sep = ""))) +
+  ylab(expression(paste(italic("E. virginicus"), " adult biomass (g ", m^-2, ")", sep = ""))) +
+  scale_color_viridis_d(end = 0.6) +
+  scale_fill_viridis_d(end = 0.6) +
+  fig_theme +
+  theme(axis.title.x = element_text(size = 10, hjust = 0.75))
+
+pdf("output/plot_biomass_density_2019_density_exp.pdf", width = 7, height = 3)
+plot_grid(mv_fig, evS_fig, evA_fig,
+          nrow = 1,
+          labels = LETTERS[1:3])
+dev.off()
+
+
 #### values for text ####
 
 posterior_samples(mvBioDensMod) %>%
@@ -353,7 +534,7 @@ posterior_samples(mvBioDensMod) %>%
   group_by(treatment) %>%
   mean_hdi(biomass)
 
-posterior_samples(evSBioDensMod) %>%
+posterior_samples(evSBioDensMod2) %>%
   rename(b_fungicide_density = "b_Ev_seedling_density:fungicide") %>%
   transmute(water = exp(b_Intercept + b_Ev_seedling_density * 19),
             fungicide = exp(b_Intercept + b_Ev_seedling_density * 19 + b_fungicide + b_fungicide_density * 19)) %>%
@@ -364,7 +545,7 @@ posterior_samples(evSBioDensMod) %>%
   group_by(treatment) %>%
   mean_hdi(biomass)
 
-posterior_samples(evABioDensMod) %>%
+posterior_samples(evABioDensMod2) %>%
   rename(b_fungicide_density = "b_Ev_adult_density:fungicide") %>%
   transmute(water = exp(b_Intercept + b_Ev_adult_density * 9),
             fungicide = exp(b_Intercept + b_Ev_adult_density * 9 + b_fungicide + b_fungicide_density * 9)) %>%
@@ -382,5 +563,5 @@ write_csv(plotD2Dat, "intermediate-data/plot_biomass_density_2019_density_exp.cs
 
 # models
 save(mvBioDensMod, file = "output/mv_plot_biomass_density_model_2019_density_exp.rda")
-save(evSBioDensMod, file = "output/ev_seedling_plot_biomass_density_model_2019_density_exp.rda")
+save(evSBioDensMod2, file = "output/ev_seedling_plot_biomass_density_model_2019_density_exp.rda")
 save(evABioDensMod, file = "output/ev_adult_plot_biomass_density_model_2019_density_exp.rda")
